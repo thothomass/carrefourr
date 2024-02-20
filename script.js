02f63b33c6a1fee3744dccc40f44466153b84699
thothomass <160597442+thothomass@users.noreply.github.com>
2024-02-20 12:10:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Masquer tous les contenus des onglets au chargement de la page
+var tabContents = document.getElementsByClassName("tabcontent");
+for (var i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
+}
